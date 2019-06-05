8067a0375920a49081631038f1b90e773da1dd23
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-06-05 20:51:50 +0530

    Added the images

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function () {    
+    
+    new WOW().init();    
+
+
+    /*****************Testimonial Section**************/
+    $(".customers-testimonials").owlCarousel({
+        items:1,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true,
+    });
+
+   
+});
